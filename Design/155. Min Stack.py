e286d591e4aa26df2a00bb592719fb52bbf49b1c
Danila <dikrivenkov@edu.hse.ru>
2019-03-09 00:38:31 +0300

    Design2

--- Design/155. Min Stack.py
+++ Design/155. Min Stack.py
@@ -36,10 +36,3 @@ class MinStack:
             return None
         else:
             return self.stack[-1][1]
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
