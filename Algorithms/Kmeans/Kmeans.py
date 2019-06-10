a6926504d8b10881ed1643c670138ea5af842030
A6a7H <dikrivenkov@edu.hse.ru>
2019-06-11 01:36:37 +0300

    challange_day_7

--- Algorithms/Kmeans/Kmeans.py
+++ Algorithms/Kmeans/Kmeans.py
@@ -2,35 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-class KMeans:
-
-    def __init__(self, k):
+class K_Means:
+    def __init__(self, k=2, tol=0.001, max_iter=300):
         self.k = k
-        self.means = None
+        self.tol = tol
+        self.max_iter = max_iter
 
+    def fit(self,data):
 
-    def classify(self, inputs):
-        return min(range(self.k), key=lambda i: np.linalg.norm(self.means[i] - inputs))
-
-
-
-    def train(self, inputs):
-        self.means = inputs[np.random.choice(inputs.shape[0], self.k)]
-        assignment = None
-        while True:
-            new_assignment = [self.classify(g) for g in inputs]
-            # map(self.classify, inputs)
-            if assignment == new_assignment:
-                return
-            assignment = new_assignment
+        self.centroids = {}
+        for i in range(self.k):
+            self.centroids[i] = data[i]
+        for i in range(self.max_iter):
+            self.classifications = {}
             for i in range(self.k):
-                point = np.array([p for p,a in zip(inputs, assignment) if a == i])
-                if len(point) > 0:
-                    self.means[i] = point.mean()
-            plt.plot(self.means[0][0], self.means[0][1], 'v')
-            plt.plot(self.means[1][0], self.means[1][1], 'v')
-            plt.plot(self.means[2][0], self.means[2][1], 'v')
-            plt.show()
+                self.classifications[i] = []
+            for featureset in X:
+                distances = [np.linalg.norm(featureset-self.centroids[centroid]) for centroid in self.centroids]
+                classification = distances.index(min(distances))
+                self.classifications[classification].append(featureset)
+            prev_centroids = dict(self.centroids)
+            for classification in self.classifications:
+                self.centroids[classification] = np.average(self.classifications[classification],axis=0)
+            optimized = True
+            for c in self.centroids:
+                original_centroid = prev_centroids[c]
+                current_centroid = self.centroids[c]
+                if np.sum((current_centroid-original_centroid)/original_centroid*100.0) > self.tol:
+                    print(np.sum((current_centroid-original_centroid)/original_centroid*100.0))
+                    optimized = False
+            if optimized:
+                break
+
+    def predict(self,data):
+        distances = [np.linalg.norm(data-self.centroids[centroid]) for centroid in self.centroids]
+        classification = distances.index(min(distances))
+        return classification
 
 
 
@@ -43,8 +50,12 @@ y = np.array([1]*100 + [2]*100 + [3]*100)
 plt.plot(X1[:, 0], X1[:, 1] ,'o', color='yellow')
 plt.plot(X2[:, 0], X2[:, 1], 'o', color='green')
 plt.plot(X3[:, 0], X3[:, 1], 'o', color='blue')
-k_means = KMeans(3)
-k_means.train(X)
-print(k_means.means)
+k_means = K_Means(k=3)
+k_means.fit(X)
+plt.plot(k_means.centroids[0][0], k_means.centroids[0][1], '>', color='yellow')
+plt.plot(k_means.centroids[1][0], k_means.centroids[1][1], '>', color='red')
+plt.plot(k_means.centroids[2][0], k_means.centroids[2][1], '>', color='blue')
+plt.show()
+print(k_means)
 
 
