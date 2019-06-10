a2ded123b1d2675bbfd154e0b36fd8f8336e303e
A6a7H <dikrivenkov@edu.hse.ru>
2019-06-10 23:13:54 +0300

    challange_day_5

---
+++ Algorithms/Kmeans/Kmeans.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+class KMeans:
+
+    def __init__(self, k):
+        self.k = k
+        self.means = None
+
+
+    def classify(self, inputs):
+        return min(range(self.k), key=lambda i: np.linalg.norm(self.means[i] - inputs))
+
+
+    def train(self, inputs):
+        self.means = inputs[np.random.choice(inputs.shape[0], self.k)]
+        assignment = None
+        while True:
+            new_assignment = [self.classify(g) for g in inputs]
+            # map(self.classify, inputs)
+            if assignment == new_assignment:
+                return
+            assignment = new_assignment
+            for i in range(self.k):
+                point = np.array([p for p,a in zip(inputs, assignment) if a == i])
+                if len(point) > 0:
+                    self.means[i] = point.mean()
+            plt.plot(self.means[0][0], self.means[0][1], 'v')
+            plt.plot(self.means[1][0], self.means[1][1], 'v')
+            plt.plot(self.means[2][0], self.means[2][1], 'v')
+            plt.show()
+
+
+
+np.random.seed(123)
+X1 = np.random.randn(100,2)
+X2 = np.random.randn(100,2) - np.array([10,1])
+X3 = np.random.randn(100,2) - np.array([1,10])
+X = np.vstack((X1,X2,X3))
+y = np.array([1]*100 + [2]*100 + [3]*100)
+plt.plot(X1[:, 0], X1[:, 1] ,'o', color='yellow')
+plt.plot(X2[:, 0], X2[:, 1], 'o', color='green')
+plt.plot(X3[:, 0], X3[:, 1], 'o', color='blue')
+k_means = KMeans(3)
+k_means.train(X)
+print(k_means.means)
+
+
---
+++ Binary Search/Find One unique digit.py
@@ -0,0 +1,26 @@
+'''
+Find one digit without pare in given sorted array(all digits in this array have pairs except one)
+'''
+class Solution:
+    def find_one_unique_digit(self, data):
+        l = 0
+        r = len(data) -1
+        while l < r:
+            m = (l+r) // 2
+            if data[m] != data[m+1] and data[m] != data[m-1]:
+                return data[m]
+            elif data[m] == data[m+1]:
+                if len(data[m+2:]) % 2 == 0:
+                    r = m
+                else:
+                    l = m + 2
+            elif data[m] == data[m-1]:
+                if len(data[m+1:]) % 2 == 0:
+                    r = m - 1
+                else:
+                    l = m + 1
+
+data = [0, 1, 1, 2, 2, 3, 3, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9]
+slt = Solution()
+print(slt.find_one_unique_digit(data))
+
