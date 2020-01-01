490e92877ca06baddb54e7a38e5992f22fbb627f
danjiii <danila.krivenkov@mail.ru>
2020-01-01 14:50:04 +0300

    start ebash

---
+++ tree/404. Sum of Left Leaves.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        
+        def helper(root, left_flag):
+            if not root.left and not root.right and left_flag:
+                self.total += root.val
+            if root.left:
+                helper(root.left, True)
+            if root.right:
+                helper(root.right, False)
+            return self.total
+                
+        if not root:
+            return 0
+        
+        self.total = 0
+        return helper(root, False)
+
+
+
+    def sumOfLeftLeaves_(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        if root.left :
+            if not root.left.left and not root.left.right:
+                return root.left.val + self.sumOfLeftLeaves(root.right)
+        return self.sumOfLeftLeaves(root.left)+self.sumOfLeftLeaves(root.right)
