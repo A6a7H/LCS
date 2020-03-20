f20d323d4c968a58d985930d38d86e24f67afbf6
Danila Krivenkov <37267124+A6a7H@users.noreply.github.com>
2020-03-21 00:14:40 +0300

    Update 23. Merge k Sorted Lists.py

--- Linked List/23. Merge k Sorted Lists.py
+++ Linked List/23. Merge k Sorted Lists.py
@@ -3,25 +3,21 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-
-import heapq 
+import heapq
 
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        if not lists:
-            return
-        my_heap = []
-        heapq.heapify(my_heap)
-        for idx, head in enumerate(lists):
-            if not head:
-                continue
-            heapq.heappush(my_heap, (head.val, idx, head))
+        heap = []
+        for list in lists:
+            while(list):
+                heap.append(list.val)
+                list = list.next
         
-        fake_head = ListNode(None)
-        cur = fake_head
-        while my_heap:
-            nxt_val, idx, cur.next = heapq.heappop(my_heap)
-            if cur.next.next:
-                heapq.heappush(my_heap, (cur.next.next.val, idx, cur.next.next))
-            cur = cur.next
-        return fake_head.next
+        heapq.heapify(heap)
+        preHead = ListNode(0)
+        tmp = preHead
+        while heap:
+            tmp.next = ListNode(heapq.heappop(heap))
+            tmp = tmp.next
+        
+        return preHead.next
