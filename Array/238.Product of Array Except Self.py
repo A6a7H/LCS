2434277114746118c7419754a2296989b5bbc2eb
Danila Krivenkov <37267124+A6a7H@users.noreply.github.com>
2020-04-10 22:42:41 +0300

    Update 238.Product of Array Except Self.py

--- Array/238.Product of Array Except Self.py
+++ Array/238.Product of Array Except Self.py
@@ -1,10 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        output = [1] * len(nums)
-        prev_left, prev_right = 1, 1
-        for i in range(1, len(nums)):
-            prev_left *= nums[i - 1]
-            prev_right *= nums[len(nums) - i]
-            output[i] *= prev_left
-            output[len(nums) - i - 1] *= prev_right
+        arr_f = [1]
+        arr_b = [1]
+        output = []
+        for i in range(len(nums)-1):
+            arr_f.append(arr_f[-1]*nums[i])
+        for i in range(len(nums)-1, 0, -1):
+            arr_b.append(arr_b[-1]*nums[i])
+        for i in range(len(arr_b)):
+            output.append(arr_f[i]*arr_b[len(arr_b)-1-i])
+        return output
+    
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        output = [1]
+        for i in range(len(nums)-1):
+            output.append(output[-1]*nums[i])
+        r = 1
+        for i in range(len(nums)-1, -1, -1):
+            output[i] *= r
+            r *= nums[i]
+            arr_b.append(arr_b[-1]*nums[i])
+        for i in range(len(arr_b)):
+            output.append(arr_f[i]*arr_b[len(arr_b)-1-i])
         return output
