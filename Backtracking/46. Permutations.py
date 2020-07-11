e9928499e30b6d8adc8f3b75f8b26d4f2ea2f1fa
Danila Krivenkov <37267124+A6a7H@users.noreply.github.com>
2020-07-12 00:24:23 +0300

    Update 46. Permutations.py

--- Backtracking/46. Permutations.py
+++ Backtracking/46. Permutations.py
@@ -1,13 +1,11 @@
-class Solution(object):
-    def permute(self, nums):
-        res = []
-        self.permute_req(nums,[],res)
-        return(res)
-        
-    def permute_req(self,nums,cur,res):
-        if (len(nums) == 0):
-            res.append(cur)
-            return
-        for i in range(len(nums)):
-            nums[0],nums[i] = nums[i],nums[0]
-            self.permute_req(nums[1:], cur+[nums[0]], res)
+def permute(self, nums: List[int]) -> List[List[int]]:
+    result = []
+    self.backtrack(nums, [], result)
+    return result
+
+def backtrack(self, nums, currentPermutation, result):
+    if not nums:
+        result.append(currentPermutation)
+        return
+    for i in range(len(nums)):
+        self.backtrack(nums[:i] + nums[i+1:], currentPermutation + [nums[i]], result)
